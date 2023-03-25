c50a995950bb284adc440375e5e259b751bb68e0
okwell-me <46261997+okwell-me@users.noreply.github.com>
2023-03-25 15:24:51 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import csv
 
 def start_my_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,13 +15,18 @@ def start_my_server():
     print('Stop')
 
 def process_data(data):
-    message = data.split(' ')[1]
-    message = message.split(';')
-    msg_type = message[0]
-    print('message type: ')
-    if msg_type == '/DAT':
-        print('data')
-    if msg_type == '/SYS':
-        print('system')
+    if len(data.split(' ')) > 1:
+        message = data.split(' ')[1]
+        message = message.split(';')
+        msg_count = len(message)
+        msg_type = message[0]
+        print('message type: ')
+        if msg_type == '/DAT':
+            print('data')
+        if msg_type == '/SYS':
+            print('system')
+        with open("data.csv", mode="w", encoding='utf-8') as file:
+            file_writer = csv.writer(file, delimiter=";", lineterminator="\r")
+            file_writer.writerow(message[1:len+1])
 
 start_my_server()
