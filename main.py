0aa4b470d250415ceeb76956a841ef6bad7f3da8
okwell-me <46261997+okwell-me@users.noreply.github.com>
2023-03-25 14:24:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -8,22 +8,17 @@ def start_my_server():
     while True:
         client_socket, address = server.accept()
         data = client_socket.recv(1024).decode('utf-8')
-        #content = 'Im alive ok'.encode('utf-8')
-        content = load_page_get(data)
-        client_socket.send(content)
+        process_data(data)
+        client_socket.send('OK')
         client_socket.shutdown(socket.SHUT_WR)
     print('Stop')
 
-def load_page_get(request_data):
-    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-    path = request_data.split(' ')[1]
-    response = ''
-    try:
-        with open('views'+path, 'rb') as file:
-            response = file.read()
-        return HDRS.encode('utf-8') + response
-    except FileNotFoundError:
-        return (HDRS_404 + 'Bruh.........').encode('utf-8')
+def process_data(data):
+    message_type = data.split(' ')[1]
+    print('message type: ')
+    if message_type == 'DAT':
+        print('data')
+    if message_type == 'SYS':
+        print('system')
 
 start_my_server()
